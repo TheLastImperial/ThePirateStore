47653c5e24f064e00a325b7850b601f5fe9fc351
dhartaros <frankvaldez@outlook.com>
2016-11-04 17:30:52 -0700

    Dashboard traducido

--- vendor/serverfireteam/rapyd-laravel/lang/en/rapyd.php
+++ vendor/serverfireteam/rapyd-laravel/lang/en/rapyd.php
@@ -1,26 +1,25 @@
 <?php
 
 return array(
-    'save'    => 'Save',
-    'undo'    => 'Undo',
-    'show'    => 'Show',
-    'modify'  => 'Modify',
-    'delete'  => 'Delete',
-    'add'     => 'Add',
-    'reset'   => 'Reset',
-    'search'  => 'Search',
-    'back'    => 'Back',
+    'save'    => 'Guardar',
+    'undo'    => 'Deshacer',
+    'show'    => 'Mostrar',
+    'modify'  => 'Modificar',
+    'delete'  => 'Eliminar',
+    'add'     => 'Agregar',
+    'reset'   => 'Resetear',
+    'search'  => 'Buscar',
+    'back'    => 'Regresar',
 
     //dataedit
-    'inserted'   => 'New record created.',
-    'updated'    => 'The record has been updated.',
-    'deleted'    => 'The record has been deleted.',
-    'err'        => 'Error reading record.',
-    'err_unknown'=> 'Error, no record selected',
-    'err_dup_pk' => 'Error, duplicated primary key',
-    'no_records' => 'There are no records to show.',
-    'conf_delete'=> 'Do you want to eliminate the record?',
+    'inserted'   => 'Nuevo Registro Creado',
+    'updated'    => 'El registro fue modificado.',
+    'deleted'    => 'El registro fue eliminado',
+    'err'        => 'Error leyendo el registro',
+    'err_unknown'=> 'Error, no se seleccionó el registro',
+    'err_dup_pk' => 'Error, llave primaria duplicada',
+    'conf_delete'=> '¿Realmente desea eliminar este registro?',
     'empty_list' => 'No record is added !',
-    'help'       => 'Help',
+    'help'       => 'Ayuda',
     'links_help' => "The 'Edit Links' form is used for adding a link in the sidebar menu of panel. The value of 'Display' field specifies the label of the link to be displayed in the menu and the value of 'Link' field specifies the name of the model."
 );
